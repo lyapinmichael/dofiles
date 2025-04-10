ac90e175463fed990528ab316a461a379c491e97
Michael Lyapin <lyapin.dev@gmail.com>
2025-04-10 22:50:30 +0300

    removed nasty tab bar from wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,38 +1,40 @@
 local wezterm = require("wezterm")
 
 return {
-  -- Цветовая схема
-  color_scheme = "Catppuccin Macchiato",
+	-- Цветовая схема
+	color_scheme = "Catppuccin Macchiato",
 
-  -- Прозрачность
-  window_background_opacity = 0.95,
+	-- Прозрачность
+	window_background_opacity = 0.95,
 
-  -- TERM env
-  set_environment_variables = {
-    TERM = "xterm-256color",
-  },
+	-- TERM env
+	set_environment_variables = {
+		TERM = "xterm-256color",
+	},
 
-  -- Шрифты
-  font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Regular", italic = false }),
-  font_rules = {
-    {
-      intensity = "Bold",
-      italic = false,
-      font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Bold", italic = false }),
-    },
-    {
-      intensity = "Bold",
-      italic = true,
-      font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Bold", italic = true }),
-    },
-    {
-      intensity = "Normal",
-      italic = true,
-      font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Regular", italic = true }),
-    },
-  },
-  font_size = 14.0,
+	-- Шрифты
+	font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Regular", italic = false }),
+	font_rules = {
+		{
+			intensity = "Bold",
+			italic = false,
+			font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Bold", italic = false }),
+		},
+		{
+			intensity = "Bold",
+			italic = true,
+			font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Bold", italic = true }),
+		},
+		{
+			intensity = "Normal",
+			italic = true,
+			font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Regular", italic = true }),
+		},
+	},
+	font_size = 14.0,
 
-  -- Опционально: убрать колокол
-  audible_bell = "Disabled",
+	-- Опционально: убрать колокол
+	audible_bell = "Disabled",
+
+	enable_tab_bar = false,
 }
